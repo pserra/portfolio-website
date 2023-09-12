afa1adfed3ce3d3a704e67fe207f3028588891c5
Paul Serra <paul@serra.biz>
2023-09-12 15:03:07 -0400

    Move images, update tagline

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const EXAMPLE_PATH = 'blog-starter'
 export const AUTHOR_NAME = 'Paul A. Serra'
-export const AUTHOR_TAGLINE = 'Data Engineer / Market Enthusiest'
+export const AUTHOR_TAGLINE = 'Architecting Software, Fostering Team Growth, Building Relationships'
 export const HOME_OG_IMAGE_URL =
   '/assets/space-cover.jpeg'
   export const BACKGROUND_IMAGE =
